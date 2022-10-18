9c444d3183af0c014624a1254973b271529b3c44
Lucas Kroger <lucasmkroger12@gmail.com>
2022-10-18 12:45:27 -0300

    atu projetin

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './container/Login'
+import Home from './container/Home'
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />}/>
+      <Route path="*" element={<Home />}/>  
+    </Routes>
+  )
+}
+
+export default App
